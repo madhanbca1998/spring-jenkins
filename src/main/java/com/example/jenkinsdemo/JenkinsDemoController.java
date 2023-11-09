b54ac310e5fb59c810761c668f573228c9c98230
madhanbca1998 <madhan.n@spectrum7tech.com>
2023-11-09 13:44:18 +0530

    first commit

---
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoController.java
@@ -0,0 +1,19 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsDemoController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Welcome to Jenkins";
+    }
+
+    @GetMapping("/hello")
+    public String world(){
+        return "Hello World";
+    }
+}
